a43a025f2487c7df3586f89e1fd896e1a2b405a3
joeaoregan <joe.oregan@gmail.com>
2025-11-22 02:37:55 +0000

    Q4a

---
+++ exam_papers/2024_25_summer/q4a_regex.py
@@ -0,0 +1,29 @@
+# QUESTION 4 [TOTAL MARKS: 20]
+# For this question you may assume that Python's re (regular expression) module has been imported.  You 
+# may not import anything else.
+
+# Q 4(a) [4 Marks]
+# A web developer uses the following filename convention: x.y
+# where x consists of letters, numbers, underscores and hypens (dashes) 
+#       y consists only of letters, with a maximum of 4 permitted.
+
+# Write a Python program which repeatedly inputs filenames and uses a regular expression to determine 
+# and display whether or not it is valid according to the above convention. 
+
+import re
+
+regex = r"\w+\.\w{,4}"
+
+def check_filename(filename):
+    if re.fullmatch(regex, filename):
+        return("Valid Filename")
+    return("Not a valid Filename")
+
+while True:
+    filename = input("Enter the filename or q to quit: ")
+
+    if filename == "q":
+        break
+
+    print(check_filename(filename))
+
---
+++ exam_papers/2024_25_summer/q4a_test.py
@@ -0,0 +1,14 @@
+import pytest
+from q4a_regex import check_filename
+
+def test_check_filename():
+    assert check_filename("index.html") == "Valid Filename"
+    assert check_filename("favion.ico") == "Valid Filename"
+    assert check_filename("flask_web_app100.py") == "Valid Filename"
+    assert check_filename("home page.htm") == "Not a valid Filename"
+    assert check_filename("index") == "Not a valid Filename"
+    assert check_filename("$special.png") == "Not a valid Filename"
+
+    
+if __name__=="__main__":
+    pytest.main([__file__, "-v"])
